7da2e30be3494278ab8a161e75b391855b75d054
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2022-06-20 19:50:38 -0600

    webAppMaye

---
+++ webAppMayerli/webAppMayerli/Controllers/ArticuloesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using webAppMayerli.Models;
+
+namespace webAppMayerli.Controllers
+{
+    public class ArticuloesController : Controller
+    {
+        private SuperMercadoEntities db = new SuperMercadoEntities();
+
+        // GET: Articuloes
+        public ActionResult Index()
+        {
+            return View(db.Articulo.ToList());
+        }
+
+        // GET: Articuloes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulo.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articuloes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idArticulo,nombre,descripcion,precio,cantidad,codigo,proveedor,estado")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articulo.Add(articulo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulo.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articuloes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idArticulo,nombre,descripcion,precio,cantidad,codigo,proveedor,estado")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(articulo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulo.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articuloes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Articulo articulo = db.Articulo.Find(id);
+            db.Articulo.Remove(articulo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
